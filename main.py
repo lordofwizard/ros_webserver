c59fe4c6a73e5f4c4549bb0396f127bf3d541bbd
lordofwizard <advaitpandharpurkar619@gmail.com>
2024-07-24 13:32:33 +0530

    Added yaml

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import os
 import signal
 import uvicorn
 import json 
+import yaml
 app = FastAPI()
 
 # GLOBAL PROCESSES
@@ -15,11 +16,19 @@ USBCAM = None
 ROSBAG = None
 ROSBAG_RECORD = None
 
+config_file_path = "config.yaml"
+config = ""
+
+with open(config_file_path, 'r') as file:
+    config = yaml.safe_load(file)
+
+
+
 def kill(pid):
     os.kill(pid, signal.SIGTERM)
 
 def start_proc(command: str, package_name: str, launch_file: str) -> subprocess.Popen:
-    command = f"source /opt/ros/noetic/setup.bash && source /home/tortoisebot/ros1_ws/devel/setup.bash && {command} {package_name} {launch_file}"
+    command = f"source {config["ros"]["ros_setup"]} && source {config["ros"]["workspace_setup"]} && {command} {package_name} {launch_file}"
     proc = subprocess.Popen([
         command
     ],
@@ -28,7 +37,9 @@ def start_proc(command: str, package_name: str, launch_file: str) -> subprocess.
     return proc
 
 def check_or_create_bags_directory():
-    directory = "bags"
+    #directory = "bags"
+    directory = config["rosbag"]["save_directory"]
+
     if not os.path.exists(directory):
         os.makedirs(directory)
         print(f"Directory for Bags already '{directory}' created.")
@@ -62,10 +73,10 @@ async def rosbag_play(file_name: str = Form(...), speed: Optional[int] = Form(No
     if os.path.exists("./bags/"+file_name+".bag") == False:
         return JSONResponse(content={'error': f'{file_name}.bag file not found'}, status_code=404)
     elif speed is not None:
-        ROSBAG = subprocess.Popen([f"source /opt/ros/noetic/setup.bash && source ~/ros1_ws/devel/setup.bash && rosbag play ./bags/{file_name}.bag -r {speed}"],shell=True, executable="/bin/bash")
+        ROSBAG = subprocess.Popen([f"source {config["ros"]["ros_setup"]} && source {config["ros"]["workspace_setup"]} && rosbag play ./bags/{file_name}.bag -r {speed}"],shell=True, executable="/bin/bash")
         return JSONResponse(content={'message': f'rosbag play started with rate={speed}:'}, status_code=200)
     else:
-        ROSBAG = subprocess.Popen([f"source /opt/ros/noetic/setup.bash && source ~/ros1_ws/devel/setup.bash && rosbag play ./bags/{file_name}.bag"],shell=True, executable="/bin/bash")
+        ROSBAG = subprocess.Popen([f"source {config["ros"]["ros_setup"]} && source {config["ros"]["workspace_setup"]} && rosbag play ./bags/{file_name}.bag"],shell=True, executable="/bin/bash")
         return JSONResponse(content={'message': 'rosbag play started'}, status_code=200)
     return JSONResponse(content={'message': 'Name received:'}, status_code=200)
 
@@ -85,7 +96,7 @@ async def start_tcp():
     if STARTTCP is not None:
         return JSONResponse(content={'message': 'Already TCP End Point Running'}, status_code=409)
     else:
-        STARTTCP = start_proc("roslaunch", "ros_tcp_endpoint", "endpoint.launch")
+        STARTTCP = start_proc("roslaunch", config["start_tcp"]["package_name"], config["start_tcp"]["launch_file"])
     return JSONResponse(content={'message': 'Starting TCP'}, status_code=200)
 
 @app.get("/stop_tcp")
@@ -105,7 +116,7 @@ async def usb_cam():
     if USBCAM is not None:
         return JSONResponse(content={'message':'USB Cam Process found, stop already running process'}, status_code=409)
     else:
-        USBCAM = start_proc("roslaunch", "usb_cam", "2_usb_cam.launch")
+        USBCAM = start_proc("roslaunch", config["usb_cam"]["package_name"], config["usb_cam"]["launch_file"])
         #return JSONResponse(content={'message':'No Camera Process found'}, status_code=404)
     return JSONResponse(content={'message': 'Starting USB Cam'}, status_code=200)
 
@@ -126,7 +137,7 @@ async def robot_bringup():
     if BRINGUP is not None:
         return JSONResponse(content={'message': 'Bringup already running'}, status_code=409)
     else:
-        BRINGUP = start_proc("roslaunch", "tortoisebot_firmware", "bringup.launch")
+        BRINGUP = start_proc("roslaunch", config["bringup"]["package_name"], config["bringup"]["launch_file"])
     return JSONResponse(content={'message': 'Starting Robot Bringup'}, status_code=200)
 
 @app.get("/stop_bringup")
@@ -152,7 +163,7 @@ async def start_rosbag_record(file_name: str = Form(...), topics: str = Form(...
         return JSONResponse(content={'error': 'Recording Already Running'}, status_code=409)
     else:
         if BRINGUP is not None:
-            ROSBAG_RECORD = subprocess.Popen([f"source /opt/ros/noetic/setup.bash && source ~/ros1_ws/devel/setup.bash && rosbag record -a -O ./bags/{file_name}.bag {topics}"],shell=True, executable="/bin/bash")
+            ROSBAG_RECORD = subprocess.Popen([f"source {config["ros"]["ros_setup"]} && source {config["ros"]["workspace_setup"]} && rosbag record -a -O ./bags/{file_name}.bag {topics}"],shell=True, executable="/bin/bash")
         else:
             return JSONResponse(content={'message': 'No Bringup Running found'}, status_code=404)
     return JSONResponse(content={'message': 'Starting RosBag Record'}, status_code=200)
@@ -169,7 +180,8 @@ async def stop_rosbag_record():
 
 @app.get("/rosbag_list")
 async def rosbag_list():
-    directory = "bags"
+    #directory = "bags"
+    directory = config["rosbag"]["save_directory"]
     if not os.path.exists(directory):
         return JSONResponse(content=json.dumps({"state": "absent-dir"}), status_code = 404)
     files = os.listdir(directory)
